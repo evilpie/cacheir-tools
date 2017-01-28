95dc8d59bcd594f2559aae395263bdd480131e27
Tom Schuster <evilpies@gmail.com>
2017-01-28 14:33:43 +0100

    modify fix-json to include option to filer url and shorten included strings

--- fix-json.py
+++ fix-json.py
@@ -1,5 +1,38 @@
+import argparse
 import json
 
-f = open("/tmp/ic.log", "r")
-data = json.loads("[" +  f.read() + "]")
-print data
+parser = argparse.ArgumentParser(description='Process a CacheIR json file')
+parser.add_argument('json', type=argparse.FileType('r'),
+                    help='CacheIR json file to optimize')
+parser.add_argument('out', type=argparse.FileType('w'),
+                    help='Output')
+parser.add_argument('--url', type=str,
+                    help='Filter to a specific URL')
+parser.add_argument('--shorten', action='store_true',
+                    help='Shorten strings')
+
+
+args = parser.parse_args()
+data = json.load(args.json)
+
+def shorten(ref):
+    if ref['type'] != 'string' or not 'value' in ref:
+        return ref
+
+    ref['value'] = ref['value'][:20]
+    return ref
+
+filtered = []
+for ic in data:
+    if args.url:
+        if not args.url.lower() in ic['file'].lower():
+            continue
+
+    if args.shorten:
+        ic['base'] = shorten(ic['base'])
+        ic['property'] = shorten(ic['property'])
+
+    filtered.append(ic)
+
+
+json.dump(filtered, args.out)
